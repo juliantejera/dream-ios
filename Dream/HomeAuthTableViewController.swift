0f40f57dbb7ec60d660c13fa27d5e26da3d26bb1
juliantejera <juliantejera@outlook.com>
2017-06-14 08:58:33 -0400

    Hide navigation bar

--- Dream/HomeAuthTableViewController.swift
+++ Dream/HomeAuthTableViewController.swift
@@ -26,6 +26,15 @@ class HomeAuthTableViewController: UITableViewController {
         }
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
     
     // MARK: - Table view data source
 
