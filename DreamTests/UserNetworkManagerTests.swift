1dcb8d82b065702ec401e4d2ec857eda89b2ca33
juliantejera <juliantejera@outlook.com>
2017-06-18 11:24:30 -0400

    Added UserNetworkManager

---
+++ DreamTests/UserNetworkManagerTests.swift
@@ -0,0 +1,50 @@
+//
+//  UserNetworkManagerTests.swift
+//  Dream
+//
+//  Created by Julian Tejera-Frias on 6/18/17.
+//  Copyright © 2017 Julian Tejera. All rights reserved.
+//
+
+import XCTest
+@testable import Dream
+
+class UserNetworkManagerTests: XCTestCase {
+    
+    var manager: UserNetworkManager!
+    var client: MockNetworkClient!
+    
+    override func setUp() {
+        super.setUp()
+        client = MockNetworkClient()
+        manager = UserNetworkManager(client: client)
+    }
+    
+    func test_findAll_whenTheRequestSucceeds_itReturnsUsers() {
+        client.response = arrayFixture(name: "user_network_manager_find_all_success")
+        
+        manager.findAll(limit: 2, radius: 100) { (result) in
+            switch result {
+            case .success(let users):
+                XCTAssertEqual(users, [User.create(), User.create()])
+            case .failure(_):
+                XCTFail()
+            }
+        }
+    }
+    
+    func test_findAll_whenTheRequestSucceedsButTheResponseCannotBeParsed_itFails() {
+        client.response = [:]
+        manager.findAll(limit: 1, radius: 100) { (result) in
+            JTAssertNetworkClientResultFails(result)
+        }
+    }
+    
+    func test_findAll_whenTheRequestFails() {
+        client.errors = ["One error"]
+        manager.findAll(limit: 1, radius: 100) { (result) in
+            JTAssertNetworkClientResultFails(result)
+        }
+    }
+    
+}
