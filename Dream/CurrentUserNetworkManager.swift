2e3d14d1a57d52cc74e1e82704b48c914e3951e3
juliantejera <juliantejera@outlook.com>
2017-07-14 17:02:28 -0400

    Added update method to manager

--- Dream/CurrentUserNetworkManager.swift
+++ Dream/CurrentUserNetworkManager.swift
@@ -12,13 +12,33 @@ struct CurrentUserNetworkManager: NetworkManager {
     
     let client: NetworkClient
     let relativePath: String
+    private let currentUserParser: CurrentUserParser
     
     init(client: NetworkClient) {
         self.client = client
         self.relativePath = "users"
+        self.currentUserParser = CurrentUserParser()
     }
     
     func update(user: CurrentUser, callback: @escaping (NetworkClientResult<CurrentUser>) -> Void) {
-        
+        let parameters = CurrentUserSerializer().serialize(from: user)
+        let updatePath = "\(path)/\(user.id)"
+        client.request(method: .put, path: updatePath, parameters: parameters) { (result) in
+            switch result {
+            case .success(let response):
+                callback(self.parseCurrentUser(response: response))
+            case .failure(let errors):
+                callback(.failure(errors))
+            }
+        }
     }
+    
+    private func parseCurrentUser(response: Any) -> NetworkClientResult<CurrentUser> {
+        if let dictionary = response as? [AnyHashable: Any], let user = currentUserParser.parse(from: dictionary) {
+            return .success(user)
+        }
+        
+        return .failure([DreamNetworkClientError.unparsableModel.localizedDescription])
+    }
+    
 }
---
+++ DreamTests/CurrentUserNetworkManagerTests.swift
@@ -0,0 +1,51 @@
+//
+//  CurrentUserNetworkManagerTests.swift
+//  DreamTests
+//
+//  Created by Julian Tejera-Frias on 7/14/17.
+//  Copyright © 2017 Julian Tejera. All rights reserved.
+//
+
+import XCTest
+@testable import Dream
+
+class CurrentUserNetworkManagerTests: XCTestCase {
+    
+    var manager: CurrentUserNetworkManager!
+    var client: MockNetworkClient!
+    var currentUser: CurrentUser!
+    
+    override func setUp() {
+        super.setUp()
+        client = MockNetworkClient()
+        manager = CurrentUserNetworkManager(client: client)
+        currentUser = CurrentUser.create()
+    }
+    
+    func test_update_whenTheRequestSucceeds_itReturnsACurrentUser() {
+        client.response = fixture(name: "current_user")
+        manager.update(user: currentUser) { (result) in
+            switch result {
+            case .success(let updatedUser):
+                XCTAssertEqual(updatedUser, self.currentUser)
+            case .failure(_):
+                XCTFail()
+            }
+        }
+    }
+    
+    func test_update_whenTheRequestSucceedsButTheResponseCannotBeParsed_itFails() {
+        client.response = [:]
+        manager.update(user: currentUser) { (result) in
+            JTAssertNetworkClientResultFails(result)
+        }
+    }
+
+    func test_update_whenTheRequestFails_itDoesNotReturnACurrentUser() {
+        client.errors = ["One error"]
+        manager.update(user: currentUser) { (result) in
+            JTAssertNetworkClientResultFails(result)
+        }
+    }
+    
+}
